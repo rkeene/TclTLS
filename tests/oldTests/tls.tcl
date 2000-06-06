f4f58c0181502b0c1cc403650266990963dfc6cf
aborr <aborr@noemail.net>
2000-06-06 18:13:20 +0000

    moved from main test directory
    FossilOrigin-Name: 88cf64aed6e7e8fa5d3802fe1cf1a51961c39b77

---
+++ tests/oldTests/tls.tcl
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 1997-2000 Matt Newman <matt@novadigm.com>
+#
+# $Header: /home/rkeene/tmp/cvs2fossil/../tcltls/tls/tls/tests/oldTests/tls.tcl,v 1.1 2000/06/06 18:13:20 aborr Exp $
+#
+set dir [file dirname [info script]]
+regsub {\.} [info tclversion] {} vshort
+if {$tcl_platform(platform) == "windows"} {
+    if {[info exists tcl_platform(debug)]} {
+	load $dir/../win/Debug$vshort/tls.dll
+    } else {
+	load $dir/../win/Release$vshort/tls.dll
+    }
+} else {
+    load [glob $dir/../unix/libtls*]
+}
+
+proc bgerror {err} {
+    global errorInfo
+    puts stderr "BG Error: $errorInfo"
+}
+
+source $dir/../library/tls.tcl
+set tls::debug 2
+
