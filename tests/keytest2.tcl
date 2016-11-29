c39b3c1b5e05d16a9aa81e9a477681a543babb77
rkeene <rkeene@noemail.net>
2016-11-29 23:46:42 +0000

    Updated key tests
    FossilOrigin-Name: baa98c9b5d55abf992fd7ddd59893607998214fe

--- tests/keytest2.tcl
+++ tests/keytest2.tcl
@@ -1,3 +1,6 @@
+#! /usr/bin/env tclsh
+
+set auto_path [linsert $auto_path 0 [file normalize [file join [file dirname [info script]] ..]]]
 package require tls
 
 set s [tls::socket 127.0.0.1 12300]
@@ -5,4 +8,3 @@ puts $s "A line"
 flush $s
 puts [join [tls::status $s] \n]
 exit
-
